4741da2022b6b4ee9f6e85383aafb63a620f90c7
radhika bhut <radhikabhut03@gmail.com>
2024-02-26 23:23:56 +0530

    act 2

--- app/src/main/java/com/example/p1/MainActivity2.java
+++ app/src/main/java/com/example/p1/MainActivity2.java
@@ -2,13 +2,30 @@ package com.example.p1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+TextView save;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        save = findViewById(R.id.save);
+
+        Intent i = getIntent();
+        String v1 = i.getStringExtra("xyz");
+        String v2 = i.getStringExtra("abc");
+
+        save.setText(v1+" "+v2);
+
+
+
+
+
+
     }
 }
