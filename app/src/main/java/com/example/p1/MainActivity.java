250e7aec136149b00794bdd911647ab49fc95415
radhika bhut <radhikabhut03@gmail.com>
2024-02-26 23:07:52 +0530

    Main

--- app/src/main/java/com/example/p1/MainActivity.java
+++ app/src/main/java/com/example/p1/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.p1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.p1.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
-
+ActivityMainBinding demo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        demo = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(demo.getRoot());
+
+        demo.submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
